556c3be3c8efcff1027fd7db16e3097798d579bc
刘传 <yangtuolc@gmail.com>
2017-04-03 16:44:39 +0800

    implement groups#create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,18 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title)
+  end 
+
+
 end
